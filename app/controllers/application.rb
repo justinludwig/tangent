3400313390d2f80479165f6ddb34b5b374d32ad3
Justin Ludwig <justin@ldwg.us>
2008-08-13 00:45:14 +0000

    add audit logging (Issue9)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -22,6 +22,8 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include PrivilegedSystem
+
+  before_filter :audit_request
   
   # sliding_sessions
   REMEMBER_ME_EXPIRES = eval AppConfig.remember_me_expires
@@ -32,7 +34,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '51b7778c2d93981d22553fb1b9a42684'
-  
+
   # store person data in cookie
   def remember_person_data
       js_name = javascript_helper.escape_javascript(current_person.display_name)
@@ -77,16 +79,39 @@ class ApplicationController < ActionController::Base
   def requested_query(default = nil)
     params[:q] || default
   end
+
+  # log msg to audit log
+  def audit_log(msg)
+    unless @audit_log
+      logfile = File.open "#{RAILS_ROOT}/log/audit.log", 'a'
+      logfile.sync = true
+      @audit_log = AuditLogger.new logfile
+    end
+    @audit_log.info msg
+  end
+
+  # filter to log all non-idempotent actions
+  def audit_request
+    # filter password/password_confirmation request-parameters
+    audit_log "#{request.remote_ip} #{logged_in? ? current_person.id : 0} #{action_name} #{controller_name} #{request.path} #{request.parameters.inspect.gsub /\"password(_confirmation)?\"=>"[^\"]*\"(, )?/, ''}" unless (request.get? || request.head?)
+  end
   
   protected
   
-  def javascript_helper
-    JavaScriptHelper.instance
-  end
-        
-  class JavaScriptHelper
-    include Singleton
-    include ActionView::Helpers::JavaScriptHelper
-  end
+    def javascript_helper
+      JavaScriptHelper.instance
+    end
+          
+    class JavaScriptHelper
+      include Singleton
+      include ActionView::Helpers::JavaScriptHelper
+    end
+  
+    class AuditLogger < Logger
+      # restore message formatting in rails env
+      def format_message(severity, timestamp, progname, msg)
+        "#{timestamp.to_s :db} #{msg}\n"
+      end
+    end
 
 end
